d06c6c924848518b543b3633a234d8ef92d86eb8
Chris Malone <chrismrulz@hotmail.com>
2023-06-01 19:54:03 +1000

    add back manual CORS

--- server.js
+++ server.js
@@ -132,6 +132,29 @@ app.use('/rentdc', proxy('https://www.rent.com.au/properties', {
     return proxyReqOpts;
   },
   userResDecorator: function(proxyRes, proxyResData, req, res) {
+    res.set("Access-Control-Allow-Origin","*");
+    res.set("Access-Control-Allow-Methods","*");
+    res.set("Access-Control-Allow-Headers","*");
+    res.set("Access-Control-Allow-Credentials","true");
+    res.set("x-amz-apigw-id","");
+    res.set("x-amzn-remapped-connection","");
+    res.set("x-amz-apigw-id","");
+    res.set("x-amzn-remapped-date","");
+    res.set("x-amzn-remapped-server","");
+    res.set("x-amzn-requestid","");
+    res.set("x-amz-apigw-id","");
+    res.set("x-content-type-options","");
+    res.set("x-frame-options","");
+    res.set("x-amz-apigw-id","");
+    res.set("x-permitted-cross-domain-policies","*");
+    res.set("x-amz-apigw-id","");
+    res.set("x-powered-by","");
+    res.set("x-render-origin-server","");
+    res.set("x-request-id","");
+    res.set("x-xss-protection","");
+    res.set("referrer-policy","");
+    res.set("link","");
+    res.set("etag","");
     if (req.url.indexOf('/properties') !== -1) {
       res.set("content-type", "application/json; charset=utf-8");
       res.set("accept", "application/json");
