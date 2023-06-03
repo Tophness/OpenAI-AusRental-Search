54f1d0227c26a17c4b5a655fbf720ad3db7ba325
Chris Malone <chrismrulz@hotmail.com>
2023-06-03 21:10:46 +1000

    fix flatten domain addresses to string

--- server.js
+++ server.js
@@ -373,8 +373,8 @@ app.use('/domain', proxy('https://www.domain.com.au/rent', {
            delete trimmedData[id].listingModel.hasVideo;
            delete trimmedData[id].listingModel.promoType;
            delete trimmedData[id].listingModel.tags;
-		   if(trimmedData[id].address.street && trimmedData[id].address.suburb && trimmedData[id].address.state && trimmedData[id].address.postcode){
-		     trimmedData[id].address = `${trimmedData[id].address.street}, ${trimmedData[id].address.suburb}, ${trimmedData[id].address.state} ${trimmedData[id].address.postcode}`;
+		   if(trimmedData[id].listingModel.address.street && trimmedData[id].listingModel.address.suburb && trimmedData[id].listingModel.address.state && trimmedData[id].listingModel.address.postcode){
+		     trimmedData[id].listingModel.address = `${trimmedData[id].listingModel.address.street}, ${trimmedData[id].listingModel.address.suburb}, ${trimmedData[id].listingModel.address.state} ${trimmedData[id].listingModel.address.postcode}`;
 		   }
          }
         }
