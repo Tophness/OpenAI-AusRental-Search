5f25983766c497792ed076eae37741f9e4a99298
Chris Malone <chrismrulz@hotmail.com>
2023-06-03 21:37:03 +1000

    fix delete more redundant crap from domain 2

--- server.js
+++ server.js
@@ -373,23 +373,23 @@ app.use('/domain', proxy('https://www.domain.com.au/rent', {
            delete trimmedData[id].listingModel.hasVideo;
            delete trimmedData[id].listingModel.promoType;
            delete trimmedData[id].listingModel.tags;
-		   if(trimmedData[id].listingModel.address.street && trimmedData[id].listingModel.address.suburb && trimmedData[id].listingModel.address.state && trimmedData[id].listingModel.address.postcode){
+		   if(trimmedData[id].listingModel.address.hasOwnProperty('street') && trimmedData[id].listingModel.address.hasOwnProperty('suburb') && trimmedData[id].listingModel.address.hasOwnProperty('state') && trimmedData[id].listingModel.address.hasOwnProperty('postcode')){
 		     trimmedData[id].listingModel.address = `${trimmedData[id].listingModel.address.street}, ${trimmedData[id].listingModel.address.suburb}, ${trimmedData[id].listingModel.address.state} ${trimmedData[id].listingModel.address.postcode}`;
 		   }
 		   if(trimmedData[id].listingModel.features){
-			 if(trimmedData[id].listingModel.features.propertyType){
+			 if(trimmedData[id].listingModel.features.hasOwnProperty('propertyType')){
 		       delete trimmedData[id].listingModel.features.propertyType;
 			 }
-			 if(trimmedData[id].listingModel.features.isRural){
+			 if(trimmedData[id].listingModel.features.hasOwnProperty('isRural')){
 		       delete trimmedData[id].listingModel.features.isRural;
 			 }
-			 if(trimmedData[id].listingModel.features.landSize){
+			 if(trimmedData[id].listingModel.features.hasOwnProperty('landSize')){
 		       delete trimmedData[id].listingModel.features.landSize;
 			 }
-			 if(trimmedData[id].listingModel.features.landUnit){
+			 if(trimmedData[id].listingModel.features.hasOwnProperty('landUnit')){
 		       delete trimmedData[id].listingModel.features.landUnit;
 			 }
-			 if(trimmedData[id].listingModel.features.isRetirement){
+			 if(trimmedData[id].listingModel.features.hasOwnProperty('isRetirement')){
 		       delete trimmedData[id].listingModel.features.isRetirement;
 			 }
 		   }
